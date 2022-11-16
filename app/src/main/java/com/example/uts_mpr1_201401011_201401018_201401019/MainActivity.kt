f9f453263822e2f6197000ade00c5977595d9957
syaripaanum <syaripaanum@gmail.com>
2022-11-16 15:14:26 +0700

    Menambahkan kolom username dan password serta mengubah title MainActivity

--- app/src/main/java/com/example/uts_mpr1_201401011_201401018_201401019/MainActivity.kt
+++ app/src/main/java/com/example/uts_mpr1_201401011_201401018_201401019/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.uts_mpr1_201401011_201401018_201401019
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.app.AppCompatActivity
 import com.example.uts_mpr1_201401011_201401018_201401019.databinding.ActivityAppBarBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "LOGIN"
 
 
 
